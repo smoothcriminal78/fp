9ceea32e0d2ab6f3cfd02dfe1b9e0022069c0220
smoothcriminal <dim.zaycev@gmail.com>
2017-07-21 19:15:57 +0300

    initial commit

--- Board.scala
+++ Board.scala
@@ -1,6 +1,20 @@
+import scala.math._
+
 class Board(val rows: Int, val cols: Int) {
 
+  def this(board: Board) = {
+    new Board(rows, co
+  }
+
   class Tile(val x: Int, val y: Int) {
+
+    def value() = Board.this.tiles(x)(y)
+
+    def allMoves() = {
+      for(dx <- -1 to 1; dy <- -1 to 1; if((x + dx >= 0 && x + dx < rows) && (y + dy >= 0 && y + dy < cols) && abs(dx) + abs(dy) == 1)) yield {
+          new Tile(x + dx, y + dy)
+      }
+    }
 
     override def equals(that: Any): Boolean = {
       if (that.isInstanceOf[Tile]) this.hashCode == that.hashCode else false
@@ -16,8 +30,19 @@ class Board(val rows: Int, val cols: Int) {
   val tiles = ((1 to ((cols * rows)-1)) :+ 0).grouped(cols).toList
   val blank = new Tile(rows-1, cols-1)
 
+  def tile(x: Int, y: Int): Tile = {
+    new Tile(x, y)
+  }
+
   def allTilePos() = {
     for(i <- 0 until rows; j <- 0 until cols) yield (i, j)
+  }
+
+  def allAdjBoards(tile: Tile) = {
+    
+  }
+
+  def move(tile: Tile) = {
   }
 
   override def equals(that: Any): Boolean = {
@@ -32,5 +57,9 @@ class Board(val rows: Int, val cols: Int) {
 }
 
 object Board {
-
+  def main(args: Array[String] = Array()) = {
+    val b = new Board(3, 5)
+    val t = b.tile(1, 1)
+    println(t.allMoves())
+  }
 }
